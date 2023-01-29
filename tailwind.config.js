2bd17ede9bfa661924158b64969fb708cb3ceb78
Görkem Aslan <gorkemaslan@aof.anadolu.edu.tr>
2023-01-30 01:14:26 +0300

    Load up

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		colors: {
+			blue: "#1fb6ff",
+			purple: "#7e5bef",
+			pink: "#ff49db",
+			orange: "#ff7849",
+			green: "#13ce66",
+			yellow: "#ffc82c",
+			"gray-dark": "#273444",
+			gray: "#8492a6",
+			"gray-light": "#d3dce6",
+			white: "#ffffff",
+		},
+		fontFamily: {
+			sans: ["Roboto", "sans-serif"],
+			serif: ["Merriweather", "serif"],
+		},
+		extend: {
+			spacing: {
+				128: "32rem",
+				144: "36rem",
+			},
+			borderRadius: {
+				"4xl": "2rem",
+			},
+		},
+	},
+	plugins: [],
+}
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+	entry: "./src/main.tsx",
+	devtool: "inline-source-map",
+	devServer: {
+		port: "3000",
+		static: {
+			directory: path.join(__dirname, "public"),
+		},
+		open: true,
+		hot: true,
+		liveReload: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.json$/,
+				use: "json-loader",
+			},
+			{
+				test: /\.(js)x?$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
+			},
+			{
+				test: /\.(ts)x?$/,
+				exclude: /node_modules|\.d\.ts$/,
+				use: {
+					loader: "ts-loader",
+					options: {
+						compilerOptions: {
+							noEmit: false,
+						},
+					},
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader", "postcss-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+				exclude: /node_modules/,
+				use: ["file-loader?name=[name].[ext]"],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "public", "index.html"),
+			favicon: "./public/favicon.ico",
+			filename: "index.html",
+			manifest: "./public/manifest.json",
+		}),
+	],
+}
